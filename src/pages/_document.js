2f9a682ab8cbccd5c1a95f78748afbaf72e34171
Alex <astoyanov@enetpulse.com>
2026-04-17 14:24:44 +0300

    add meta tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="gwgHS__jw-RQB-1RQzKjGQtvzKeFejcrMLnGYGlNwhU"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
