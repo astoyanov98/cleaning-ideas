82060ec68e3960a8c7e1adeda47af22e3e561e13
Alex <astoyanov@enetpulse.com>
2026-04-17 14:32:09 +0300

    add googletag manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,20 @@ export default function Document() {
           name="google-site-verification"
           content="gwgHS__jw-RQB-1RQzKjGQtvzKeFejcrMLnGYGlNwhU"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-QJDF9JZS0H"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QJDF9JZS0H');
+            `,
+          }}
+        />
       </Head>
       <body className="antialiased">
         <Main />
